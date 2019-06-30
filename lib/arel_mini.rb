f1ce472ed8fc152c74109a07ff9fa6a75bc5373f
Leonardo Tolosa <tolosa@gmail.com>
2019-06-29 22:13:26 -0300

    Rename folder

--- lib/arel_mini.rb
+++ lib/arel_mini.rb
@@ -1,7 +1,7 @@
 require 'arel_mini/version'
 
-require 'arel_mini/fragments/base'
-require 'arel_mini/fragments/select'
-require 'arel_mini/fragments/from'
+require 'arel_mini/nodes/base'
+require 'arel_mini/nodes/select'
+require 'arel_mini/nodes/from'
 
 require 'arel_mini/query'
